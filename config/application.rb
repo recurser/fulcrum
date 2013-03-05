2c5d9921b046bb4f3912fce899c78e082aa86d5e
Dave Perrett <dave@recurser.com>
2013-03-06 01:32:36 +1300

    Update devise styles and add custom devise layout.

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,15 @@ module Fulcrum
 
     # Do not initialize the whole Rails stack when precompiling assets
     config.assets.initialize_on_precompile = false
+    
+    # Custom devise layout.
+    config.to_prepare do
+      Devise::SessionsController.layout      'devise'
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? 'application' : 'devise' }
+      Devise::ConfirmationsController.layout 'devise'
+      Devise::UnlocksController.layout       'devise'            
+      Devise::PasswordsController.layout     'devise'        
+    end
 
     load Rails.root.join('config','fulcrum_defaults.rb')
     if File.exist?(Rails.root.join('config', 'fulcrum.rb'))
